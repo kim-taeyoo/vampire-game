313c479c87c6b73b3695466d576382a337aaacc1
kim-taeyoo <kty0627@ajou.ac.kr>
2023-12-16 13:03:53 +0900

    merge bug fix

---
+++ vampire_game/Player/Dash.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var dashTimer = $DashTimer
+
+func startDash(duration):
+	dashTimer.wait_time = duration
+	dashTimer.start()
+	
+func isDashing():
+	return !dashTimer.is_stopped()
