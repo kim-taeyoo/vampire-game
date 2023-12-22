70ee9b3b8948d148d455f2d73595866f0581cafa
kwj <127395018+ResistingOhm@users.noreply.github.com>
2023-12-22 14:16:25 +0900

    add sprite.import file

--- vampire_game/Gui/floating_menu/OptionMenu/OptionMenu.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_button_pressed():
-	queue_free()
--- vampire_game/TitleScreen/title.gd
+++ vampire_game/TitleScreen/title.gd
@@ -3,10 +3,15 @@ extends Node2D
 @export var startGame: PackedScene
 @export var OptionMenu: PackedScene
 
+@onready var clickSound = $ButtonClick
+
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(startGame)
-
 
 func _on_option_pressed():
 	var M = OptionMenu.instantiate()
 	$CanvasLayer.add_child(M)
+	clickSound.play(0)
+
+func _on_credit_pressed():
+	clickSound.play(0)
